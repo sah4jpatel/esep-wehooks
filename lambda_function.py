77f8059594bbae942467591e9eeb0af77fd70474
Sahaj Patel <74474849+sah4jpatel@users.noreply.github.com>
2022-10-14 10:56:33 -0400

    update lambda return to slack

--- lambda_function.py
+++ lambda_function.py
@@ -8,7 +8,12 @@ def lambda_handler(event, context):
     ret_obj = {"text": event["issue"]["html_url"]}
     response = requests.post(
         url,
-        json={"text": "Issue Created: " + str(event["issue"]["html_url"])},
+        json={
+            "text": "Issue "
+            + str(event["action"]).capitalize()
+            + ": "
+            + str(event["issue"]["html_url"])
+        },
         headers={"Content-Type": "application/json"},
     )
     return {"message": response.text}
