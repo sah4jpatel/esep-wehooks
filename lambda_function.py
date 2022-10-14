f8a065846d022b33da06df1f78d177bb80f0595b
Sahaj Patel <74474849+sah4jpatel@users.noreply.github.com>
2022-10-14 08:42:33 -0400

    no link here :)

---
+++ lambda_function.py
@@ -0,0 +1,14 @@
+import json
+import requests
+
+
+def lambda_handler(event, context):
+    message = "{}".format(event["issue"]["html_url"])
+    url = "Some link here"
+    ret_obj = {"text": event["issue"]["html_url"]}
+    response = requests.post(
+        url,
+        json={"text": "Issue Created: " + str(event["issue"]["html_url"])},
+        headers={"Content-Type": "application/json"},
+    )
+    return {"message": response.text}
